b98d274efefda0fc65a1fe10995a77ecd0c3d495
Toria-T <vickymariet@comcast.net>
2022-02-10 03:11:03 -0500

    Updated Unity Project
    Added new assets: PhysicsCube and TriggerCube. Added a CollisionDetectionScript for the TriggerCubes to keep track of interactions.

---
+++ IntroGameProgrammingWeek2/Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+    public float hitCounter; //keeps track of collision events
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        hitCounter += 1; //updates total times interacted
+        print("I've been hit " + hitCounter + " times."); //prints the total number of interactions
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
