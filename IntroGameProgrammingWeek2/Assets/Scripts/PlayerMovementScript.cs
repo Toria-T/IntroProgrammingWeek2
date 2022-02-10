4b783fc294db275bc68f3bac699bb1bfb34bafa5
Toria-T <vickymariet@comcast.net>
2022-02-10 00:53:50 -0500

    Created Unity Project
    Created the base Unity project, and created some materials, some objects, and a script

---
+++ IntroGameProgrammingWeek2/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    private float moveSpeed; //controls player speed
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D)) //if the player hits the D key, the object moves right
+        {
+            transform.position += Time.deltaTime * moveSpeed * Vector3.right;
+        }
+        if(Input.GetKey(KeyCode.A)) //if the player hits the A key, the object moves left
+        {
+            transform.position += Time.deltaTime * moveSpeed * Vector3.left;
+        }
+        if(Input.GetKey(KeyCode.W)) //if the player hits the W key, the object moves forward
+        {
+            transform.position += Time.deltaTime * moveSpeed * Vector3.forward;
+        }
+        if(Input.GetKey(KeyCode.S)) //if the player hits the S key, the object moves backwards
+        {
+            transform.position += Time.deltaTime * moveSpeed * Vector3.back;
+        }
+    }
+}
