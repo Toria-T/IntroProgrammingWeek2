5d5a1c1ce3bfd3e6afb5eeef1d648ffbf1c6488b
Toria-T <vickymariet@comcast.net>
2022-03-08 09:42:58 -0500

    Changed Main Scene Name
    Changed the scene "Main" to "TonstadMainScene"
    
    Other changes made, such as adding scripts and making said Main scene.

--- IntroGameProgrammingWeek2/Assets/Scripts/CollisionDetector.cs
+++ IntroGameProgrammingWeek2/Assets/Scripts/CollisionDetector.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CollisionDetector : MonoBehaviour
 {
     public float hitCounter; //keeps track of collision events
+    public float winState; //activates game over event
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,11 @@ public class CollisionDetector : MonoBehaviour
     {
         hitCounter += 1; //updates total times interacted
         print("I've been hit " + hitCounter + " times."); //prints the total number of interactions
+
+        if(hitCounter == 4)
+        {
+            fORCEeRROR;
+        }
     }
 
     // Update is called once per frame
